5a108393554c3ed70b9319ff98bec7c19205b63c
Brandon Gonzalez <brajgon@gmail.com>
2024-02-24 16:59:34 -0700

    server/index.js

--- server/index.js
+++ server/index.js
@@ -18,13 +18,11 @@ app.use(express.static(__dirname + "/static"));
 app.get("/api/houses", getHouses);
 app.post("/api/houses", createHouse);
 app.delete("/api/houses/:id", deleteHouse);
-app.put('/api/houses/:id', updateHouse)
+app.put("/api/houses/:id", updateHouse);
 
 app.get("/", (req, res) => {
   res.sendFile("static/index.html", { root: __dirname });
 });
 ////GET,POST,DELETE,PUT ABOVE
-
-
 
 app.listen(4000, () => console.log(`Server running on port 4000`));
